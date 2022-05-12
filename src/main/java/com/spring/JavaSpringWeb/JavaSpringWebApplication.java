91ee0ecfb7c5ff723d30a73d7740e0fe8cdfcfab
PersesTitan <torampersestitan@gmail.com>
2022-05-12 18:52:43 +0900

    Initial commit

---
+++ src/main/java/com/spring/JavaSpringWeb/JavaSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.spring.JavaSpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/JavaSpringWeb/JavaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.JavaSpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
